a4236ef71c7881466017be04ac22acfd1215904d
shamilv05 <shamilv05@gmail.com>
2018-08-02 15:24:46 +0300

    Current Time Simple Programm

---
+++ src/ShamilVagabov/CurrentTime.java
@@ -0,0 +1,31 @@
+package ShamilVagabov;
+
+public class CurrentTime {
+    public static void main(String[] args) {
+        currentTime();
+    }
+
+    public static void currentTime(){
+        //obtain the total milliseconds since Januare 1, 1970 from midnight
+        long totalMilliSeconds = System.currentTimeMillis();
+
+        long totalSeconds = totalMilliSeconds / 1000;
+        long currentSeconds = totalSeconds % 60;
+
+        long totalMinutes = totalSeconds / 60;
+        long currentMinutes = totalMinutes % 60;
+
+        long totalHours = totalMinutes / 60;
+        long currentHours = totalHours % 24;
+
+        System.out.println("Current time is " + currentHours +
+                ":" + currentMinutes + ":" + currentSeconds + " GTM");
+
+        System.out.println("Current time in Moscow is "+ (currentHours + 3) +
+                ":" + currentMinutes + ":" + currentSeconds + " GTM");
+    }
+
+
+
+
+}
